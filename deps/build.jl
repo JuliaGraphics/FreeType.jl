121d856dc41fb0217b53c66bb58447e12ef3e2d3
SimonDanisch <sdanisch@gmail.com>
2017-02-10 15:08:28 -0500

    drop 0.4, use BinDeps

--- deps/build.jl
+++ deps/build.jl
@@ -1,28 +1,17 @@
-using Compat
+using BinDeps
+@BinDeps.setup
+freetype = library_dependency("freetype", aliases = ["libfreetype", "libfreetype6", "libfreetype-6", "libfreetype.6", "freetype"])
 
-if is_windows()
+@static if is_apple()
+    using Homebrew
+    provides(Homebrew.HB, "freetype", freetype, os = :Darwin )
+end
+@static if is_windows()
     using WinRPM
-    WinRPM.install("libfreetype6", yes=true)
+    provides(WinRPM.RPM, "freetype", freetype, os = :Windows)
+end
+@static if is_linux()
+    provides(AptGet, "freetype", freetype, os = :Linux)
 end
 
-const freetype = Libdl.find_library(
-    ["libfreetype", "libfreetype-6", "libfreetype.6", "freetype"],
-    [
-        "/usr/lib/i386-linux-gnu", "/usr/lib/x86_64-linux-gnu",
-        Pkg.dir("WinRPM", "deps", "usr", "$(Sys.ARCH)-w64-mingw32", "sys-root", "mingw", "bin"),
-        "/Developer/SDKs/MacOSX10.7.sdk/usr/X11/include/freetype2", "/usr/X11/lib/",
-        "/opt/local/lib/", "/usr/local/lib/", "/usr/X11R6/lib/"
-    ]
-)
-
-
-if isempty(freetype)
-    error("FreeType library could not be found. Please file an issue here: https://github.com/jhasse/FreeType.jl/issues/new")
-end
-
-# Save lib and version into seperate file to statically include the library with precompile
-f = open("deps.jl", "w")
-write(f, """
-const freetype = \"$(escape_string(freetype))\"
-""")
-close(f)
+@BinDeps.install Dict("freetype" => "freetype")
