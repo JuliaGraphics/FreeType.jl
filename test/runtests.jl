99c88e1aff2afd520791ddf4ba617c9e12388b59
SimonDanisch <sdanisch@gmail.com>
2019-01-29 15:17:45 +0100

    directly use FreeTypeAbstraction tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
-using FreeType
+using FreeType, FreeTypeAbstraction
 using Test
 using Pkg
 library = Ref{FT_Library}()
 error = FT_Init_FreeType(library)
 @test error == 0
 
-# since there is no meaningful tests, we simply run FreeTypeAbstraction's tests for now
-Pkg.pkg"test FreeTypeAbstraction"
+# since there are no meaningful tests, we simply run FreeTypeAbstraction's tests for now
+include(joinpath(dirname(pathof(FreeTypeAbstraction)), "..", "test", "runtests.jl"))
