06a00f970a44970dfe273f5d7a02eb81c3fec376
WooKyoung Noh <wookay.noh@gmail.com>
2019-01-31 22:53:18 +0900

    GC.@preserve outline_funcs funcs

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,10 +45,12 @@ line_f = @cfunction $line_to_func Cint (Ptr{FT_Vector}, Ptr{Cvoid})
 conic_f = @cfunction $conic_to_func Cint (Ptr{FT_Vector}, Ptr{FT_Vector}, Ptr{Cvoid})
 cubic_f = @cfunction $cubic_to_func Cint (Ptr{FT_Vector}, Ptr{FT_Vector}, Ptr{FT_Vector}, Ptr{Cvoid})
 
+GC.@preserve move_f line_f conic_f cubic_f begin
 face = unsafe_load(refface[])
 glyph = unsafe_load(face.glyph)
 outline_funcs = FreeType.FT_Outline_Funcs(Base.unsafe_convert.(Ptr{Cvoid}, (move_f, line_f, conic_f, cubic_f))..., 0, 0)
 FT_Outline_Decompose(pointer_from_objref.((Ref(glyph.outline), Ref(outline_funcs)))..., C_NULL)
+end
 
 @test paths == [(:move, (502, -29)), (:conic, (399, -29), (307, -7)), (:conic, (210, 16), (109, 61)), (:line, (109, 297)), (:conic, (200, 216), (297, 176)), (:conic, (396, 135), (499, 135)), (:conic, (566, 135), (617, 153)), (:conic, (669, 171), (698, 210)), (:conic, (754, 284), (754, 487)), (:line, (754, 1323)), (:line, (373, 1323)), (:line, (373, 1493)), (:line, (956, 1493)), (:line, (956, 487)), (:conic, (956, 343), (930, 246)), (:conic, (905, 150), (850, 89)), (:conic, (795, 28), (709, 0)), (:conic, (624, -29), (502, -29))]
 
